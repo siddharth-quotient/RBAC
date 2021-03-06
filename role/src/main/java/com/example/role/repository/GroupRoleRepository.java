258a8f054c10c2fdfef44c45a59dab83b07aba74
simehta <simehta@quotient.com>
2021-03-07 01:28:17 +0530

    Fixed the mapping flow, now whenever a entity (user/group/role) gets deleted, it's respective (user_group_mapping/group_role_mapping) is deleted aswell and consistency is maintained

--- role/src/main/java/com/example/role/repository/GroupRoleRepository.java
+++ role/src/main/java/com/example/role/repository/GroupRoleRepository.java
@@ -5,10 +5,8 @@ import com.example.role.domain.GroupRoleMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Set;
 
 @Repository
 public interface GroupRoleRepository extends JpaRepository<GroupRoleMapping, Long> {
-    Set<GroupRoleMapping> findByGroupId(Long groupId);
-    void deleteByRoleId(Long groupId);
+    void deleteByRoleId(Long roleId);
 }
